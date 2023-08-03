8605080dc3f46f28db60893f86862ab74622bf12
shreensamhi <119866077+shreensamhi@users.noreply.github.com>
2023-08-03 04:20:11 +0300

    portofolio

---
+++ web/java.js
@@ -0,0 +1,7 @@
+function myFunction() {
+    let text;
+    let person = prompt("Please enter your recommindation:");
+
+      text = " " + person + ".";
+    document.getElementById("demo").innerHTML = text;
+  }
