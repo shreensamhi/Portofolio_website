502f6677affc1dbd9f2d17b4a08640c9d6993d5c
shreensamhi <119866077+shreensamhi@users.noreply.github.com>
2023-08-27 03:03:31 +0300

    update

--- web/java.js
+++
@@ -1,7 +0,0 @@
-function myFunction() {
-    let text;
-    let person = prompt("Please enter your recommindation:");
-
-      text = " " + person + ".";
-    document.getElementById("demo").innerHTML = text;
-  }
